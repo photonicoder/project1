7b84c1ff6de144804107fddbf57af36e3fbf5329
Nishant Niraj <67960267+photonicoder@users.noreply.github.com>
2024-09-18 15:39:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 4000;
  
 app.get('/', (req, res) => {
-  res.send('Hello Worlds!');
+  res.send('Hello World!');
 });
  
 app.listen(process.env.PORT || port, async () => {
